1dc54120ad6512e3749c742a057618a16ed8f5bb
kuomuchi <42135910+kuomuchi@users.noreply.github.com>
2019-11-27 21:27:10 +0800

    add new thing

--- exam/Dice.c
+++ exam/Dice.c
@@ -1,66 +1,54 @@
 #include <stdio.h>
 
+
 int main(){
-  int x, y;
-  scanf("%d%d", &x, &y);
+  int y, x, num, i, j, dice=0, testup, testdown;
+  scanf("%d%d", &y, &x);
+  int array[y][x];
 
+    for(i=0; i<y; i++){
+      // printf("\n");
+      for(j=0; j<x; j++){
+        scanf("%d", &num);
+        array[i][j]=num;
+
+        //測試
+        // printf("%d ", num);
+      }
+    }
+
+    for(i=1; i < y-1; i++){
+      for(j=0; j < x-3; j++){
+
+        //確保4個數字都不一樣。
+        if(array[i][j] != array[i][j+1] && array[i][j] != array[i][j+2] && array[i][j] != array[i][j+3] && array[i][j+1] != array[i][j+2] && array[i][j] != array[i][j+3] && array[i][j+2] != array[i][j+3]){
+          //確保橫排的數字是可以的。
+          if(array[i][j]+array[i][j+2] == 7 && array[i][j+1]+array[i][j+3] == 7){
+
+            //確認上排的數字沒有跟中橫排重複到。
+            for(testup=j; testup <= j+3; testup++){
+
+              //確認上排的數字沒有跟中橫排重複到。
+              if(array[i-1][testup] != array[i][j] && array[i-1][testup] != array[i][j+1] && array[i-1][testup] != array[i][j+2] && array[i-1][testup] != array[i][j+3]){
+
+                //如果沒有重複，則檢查現下排。
+                for(testdown=j; testdown <= j+3; testdown++){
+
+                  //如果沒有重複，則檢查現下排。
+                  if(array[i-1][testup] + array[i+1][testdown] == 7){
+
+                    //debug
+                    // printf("\n\n%d %d\n\n", array[i-1][testup], array[i+1][testdown]);
+
+                    dice++;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+  printf("%d\n", dice);
 }
-
-// #include <stdio.h>
-// #include <stdbool.h>
-//
-// #define MAXR 1000
-// #define MAXC 1000
-//
-// #define WIDTH 4
-// #define POINT 6
-//
-// int countUpperLower(int matrix[MAXR][MAXC], int row, int col, bool appeared[])
-// {
-//   int count = 0;
-//   for (int i = 0; i < WIDTH; i++) {
-//     for (int j = 0; j < WIDTH; j++) {
-//       int upper = matrix[row - 1][col + i];
-//       int lower = matrix[row + 1][col + j];
-//       if (!appeared[upper] && !appeared[lower] &&
-//       ((upper + lower) == POINT + 1))
-//     count++;
-//     }
-//   }
-//   return count;
-// }
-//
-// int numDice(int matrix[MAXR][MAXC], int row, int col)
-// {
-//   bool appeared[POINT + 1] = {false};
-//
-//   for (int i = 0; i < WIDTH; i++)
-//     if (appeared[matrix[row][col + i]])
-//       return 0;
-//     else
-//       appeared[matrix[row][col + i]] = true;
-//
-//   if (!(matrix[row][col] + matrix[row][col + 2] == POINT + 1 &&
-//     matrix[row][col + 1] + matrix[row][col + 3] == POINT + 1))
-//     return 0;
-//   else
-//     return countUpperLower(matrix, row, col, appeared);
-// }
-//
-// int main()
-// {
-//   int R, C;
-//   int matrix[MAXR][MAXC];
-//   scanf("%d%d", &R, &C);
-//   for (int r = 0; r < R; r++)
-//     for (int c = 0; c < C; c++)
-//       scanf("%d", &matrix[r][c]);
-//
-//   int count = 0;
-//   for (int r = 1; r + 1 < R; r++)
-//     for (int c = 0; c + WIDTH <= C; c++)
-//       count += numDice(matrix, r, c);
-//
-//   printf("%d\n", count);
-//   return 0;
-// }
